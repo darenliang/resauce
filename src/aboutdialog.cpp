28923d8394e72daacc95eb8e0c2cc7f8d07d85aa
George-Lewis <georgejeffreylewis@gmail.com>
2019-12-25 02:00:16 -0500

    Clean up ui code and add website

--- src/aboutdialog.cpp
+++ src/aboutdialog.cpp
@@ -2,21 +2,23 @@
 #include "ui_aboutdialog.h"
 #include "info.h"
 
+using namespace Info;
+
 AboutDialog::AboutDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
-    QLabel* nameLabel = this->findChild<QLabel*>("nameLabel");
-    nameLabel->setText(APP_NAME);
-    QLabel* versionLabel = this->findChild<QLabel*>("versionLabel");
-    versionLabel->setText(VERSION);
-    QLabel* buildLabel = this->findChild<QLabel*>("buildLabel");
-    std::string buildLabelStr("Built with Qt 5 on ");
-    buildLabelStr.append(BUILD_DATE);
-    buildLabel->setText(buildLabelStr.c_str());
-    QLabel* authorListLabel = this->findChild<QLabel*>("authorListLabel");
-    authorListLabel->setText(AUTHORS);
+    ui->nameLabel->setText(APP_NAME);
+    ui->versionLabel->setText(VERSION);
+
+    QString build("Built with Qt 5 on ");
+
+    build.append(__DATE__);
+
+    ui->buildLabel->setText(build);
+    ui->authorLabel->setText("Authors: " + AUTHORS);
+    ui->website->setText("Website: " + WEBSITE);
 
 }
 
