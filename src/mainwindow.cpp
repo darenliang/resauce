b704b97ada587e81e1714a8a73bd4c6edaedcabf
Daren Liang <darenliang@outlook.com>
2019-12-24 23:08:50 -0500

    Upload

---
+++ src/mainwindow.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include "aboutdialog.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_actionQuit_triggered()
+{
+    QApplication::quit();
+}
+
+void MainWindow::on_actionAbout_triggered()
+{
+    AboutDialog aboutDialog;
+    aboutDialog.exec();
+
+}
