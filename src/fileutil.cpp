832ab51bb4380b0636747cbbc0cd7864808f4b75
George-Lewis <georgejeffreylewis@gmail.com>
2019-12-25 10:29:41 -0500

    Add file util classes

---
+++ src/fileutil.cpp
@@ -0,0 +1,14 @@
+#include "fileutil.h"
+
+// Instantiate
+QFileSystemModel FileUtil::model;
+
+QIcon FileUtil::getIcon(QString file) {
+
+    auto dex = model.index(file);
+
+    return model.fileIcon(dex);
+
+}
+
+QIcon FileUtil::getIcon(QFileInfo file) { return getIcon(file.path()); }
---
+++ src/fileutil.h
@@ -0,0 +1,20 @@
+#ifndef FILEUTIL_H
+#define FILEUTIL_H
+
+#include <QFileInfo>
+#include <QFileSystemModel>
+
+class FileUtil
+{
+
+    static QFileSystemModel model;
+
+    public:
+
+    static QIcon getIcon(QString file);
+
+    static QIcon getIcon(QFileInfo file);
+
+};
+
+#endif // FILEUTIL_H
