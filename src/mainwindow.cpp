9f089eb70bef71c6bca798d1ce66f06b12485a28
Daren Liang <darenliang@outlook.com>
2019-12-25 10:51:02 -0500

    Format to same style as fileutil

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -8,33 +8,26 @@
 #include <QFileInfo>
 #include <QDateTime>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
 
-void MainWindow::on_actionQuit_triggered()
-{
+void MainWindow::on_actionQuit_triggered() {
     QApplication::quit();
 }
 
-void MainWindow::on_actionAbout_triggered()
-{
+void MainWindow::on_actionAbout_triggered() {
     AboutDialog aboutDialog;
     aboutDialog.exec();
 
 }
 
-void MainWindow::on_actionOpen_triggered()
-{
+void MainWindow::on_actionOpen_triggered() {
 
     QString folderName = QFileDialog::getExistingDirectory(nullptr, ("Select Folder"), QDir::currentPath());
 
