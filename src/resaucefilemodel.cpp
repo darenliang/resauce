4a0e85dd86acde5cdb54f7d78a8671da9e268a25
George-Lewis <georgejeffreylewis@gmail.com>
2019-12-26 14:42:37 -0500

    Flesh out a few methods -- partial

--- src/resaucefilemodel.cpp
+++ src/resaucefilemodel.cpp
@@ -1,19 +1,45 @@
 #include "resaucefilemodel.h"
+#include "state.h"
+
+ResauceFileModel::ResauceFileModel() {
+
+    this->setFilter(QDir::Files | QDir::NoDotAndDotDot);
+
+}
 
 int ResauceFileModel::columnCount(const QModelIndex &parent) const {
 
-    return 0;
+    return QFileSystemModel::columnCount(parent) + 1;
 
 }
 
 QVariant ResauceFileModel::headerData(int section, Qt::Orientation orientation, int role) const {
 
-    return QVariant();
+    if (section == 1) {
+        return "New Name";
+    } else {
+        return QFileSystemModel::headerData(section, orientation, role);
+    }
 
 }
 
 QVariant ResauceFileModel::data(const QModelIndex &index, int role) const {
 
-    return QVariant();
+    if (!index.isValid() || index.model() != this)
+            return QVariant();
+
+    if (index.column() == 1) {
+
+        switch (role) {
+
+            case Qt::DisplayRole: return new_names[this->fileInfo(index).fileName()];
+
+            default: return QFileSystemModel::data(index, role);
+
+        }
+
+    } else {
+        return QFileSystemModel::data(index, role);
+    }
 
 }
--- src/resaucefilemodel.h
+++ src/resaucefilemodel.h
@@ -3,17 +3,30 @@
 
 #include <QFileSystemModel>
 #include <QVariant>
+#include <QHash>
 
 class ResauceFileModel : public QFileSystemModel
 {
 
     Q_OBJECT
 
+    QHash<QString, QString> new_names;
+
+    // Overrides
+
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
 
     QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const override;
 
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+public:
+
+    ResauceFileModel();
+
+    void put(QString& old, QString& _new);
+
+    QString& get(QString& old);
 
 };
 
