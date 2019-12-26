992cfcd5563ebada3273bf07cc8783b790533ec1
George-Lewis <georgejeffreylewis@gmail.com>
2019-12-26 07:40:40 -0500

    Remove old code

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -20,6 +20,7 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 
     // Make ui reactive to keyboard and mouse
     connect(ui->dirView->selectionModel(), &QItemSelectionModel::currentChanged, this, &MainWindow::dirView_selection_change);
+//    connect(ui->dirView, &QTreeView::keyPressEvent, ui->dirView, [](x){});
 }
 
 MainWindow::~MainWindow() {
@@ -34,56 +35,6 @@ void MainWindow::on_actionQuit_triggered() {
 void MainWindow::on_actionAbout_triggered() {
     about.exec();
 }
-
-void MainWindow::setItem(QTreeWidgetItem *item, QFileInfo &info, QFileInfo *parent = nullptr) {
-
-    qDebug() << info.absoluteFilePath();
-
-    auto path = (parent == nullptr) ? info.absoluteFilePath() : info.absoluteFilePath().remove(
-            parent->absoluteFilePath());
-
-    item->setText(0, path);
-    item->setIcon(0, FileUtil::getIcon(info));
-
-    auto size = (info.size() == 0) ? "Unavailable" : QString::number(FileUtil::getDirSize(info) / 1000000.0, 'f', 2) +
-                                                     " MB"; // Bytes to MB, round to two places
-
-    item->setText(2, size);
-    item->setText(3, (info.birthTime().isValid()) ? info.birthTime().toLocalTime().toString(Qt::DateFormat::TextDate)
-                                                  : "Unavailable");
-    item->setText(4, info.lastModified().toLocalTime().toString(Qt::DateFormat::TextDate));
-
-    if (info.isDir()) {
-
-        QDirIterator it{info.absoluteFilePath(), QDir::NoDotAndDotDot | QDir::AllEntries};
-
-        while (it.hasNext()) {
-
-            QFileInfo f{it.next()};
-
-            if (f.isDir()) {
-                continue;
-            }
-
-            setItem(new QTreeWidgetItem(item), f, &info);
-
-        }
-
-    }
-
-}
-
-/*
-void MainWindow::setFolder(const QString &folderName) {
-    State::setFolderPath(folderName);
-
-    qDebug() << "Selected: " << folderName;
-
-    QFileInfo inf{folderName};
-
-    setItem(new QTreeWidgetItem(ui->fileTree), inf);
-}
- */
 
 bool MainWindow::setDirectory(const QString &folderPath) {
     const QDir pathDir(folderPath);
