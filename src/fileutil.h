32a05d0a9c8d6c174fa2491ad3d483ff031e6921
Daren Liang <darenliang@outlook.com>
2019-12-28 15:47:53 -0500

    Add license to source files

--- src/fileutil.h
+++ src/fileutil.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2019 Daren Liang, George Lewis
+ *
+ * This file is part of Resauce.
+ *
+ * Resauce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Resauce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Resauce.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef FILEUTIL_H
 #define FILEUTIL_H
 
@@ -5,8 +25,7 @@
 #include <QFileSystemModel>
 #include <QPair>
 
-class FileUtil
-{
+class FileUtil {
 
     static QFileSystemModel model;
 
@@ -14,15 +33,15 @@ class FileUtil
 
     static QPair<double, qint8> size(double size, qint8 index = 0);
 
-    public:
+public:
 
     static QIcon getIcon(QString file);
 
-    static QIcon getIcon(QFileInfo& file);
+    static QIcon getIcon(QFileInfo &file);
 
-    static qint64 getDirSize(QFileInfo& file);
+    static qint64 getDirSize(QFileInfo &file);
 
-    static QString sizeStr(QFileInfo& info);
+    static QString sizeStr(QFileInfo &info);
 
 };
 
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2019 Daren Liang, George Lewis
+ *
+ * This file is part of Resauce.
+ *
+ * Resauce is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Resauce is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Resauce.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -12,15 +32,16 @@ QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
+class MainWindow : public QMainWindow {
+Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
 
 private slots:
+
     void on_actionQuit_triggered();
 
     void on_actionAbout_triggered();
@@ -40,10 +61,11 @@ private:
 
     AboutDialog about;
 
-    void setItem(QTreeWidgetItem* item, QFileInfo& info, QFileInfo* parent);
+    void setItem(QTreeWidgetItem *item, QFileInfo &info, QFileInfo *parent);
 
     bool setDirectory(const QString &folderPath, bool manual);
 
     void resetDirectory(bool manual);
 };
+
 #endif // MAINWINDOW_H
