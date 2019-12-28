278eb61ea7d077c0b10ec8503b401a4d17e7df7b
George-Lewis <georgejeffreylewis@gmail.com>
2019-12-28 13:26:06 -0500

    Add documentation

--- src/resaucefilemodel.cpp
+++ src/resaucefilemodel.cpp
@@ -42,32 +42,32 @@ QVariant ResauceFileModel::data(const QModelIndex &index, int role) const {
     switch(role) {
         case Qt::DisplayRole: {
             auto file = files[index.row()];
-            switch(index.column()) {
+            switch(index.column()) { // Get data by column and row
                 case 0: return file.fileName();
                 case 1: return file.new_name;
                 case 2: return FileUtil::sizeStr(file);
                 default: return QVariant();
             }
         }
-        case Qt::DecorationRole: {
-            if (index.column() == 0) {
-                return icon_provider.icon(files[index.row()]);
+        case Qt::DecorationRole: { // We need to decorate the index
+            if (index.column() == 0) { // If "Name" index
+                return icon_provider.icon(files[index.row()]); // Put an icon in there
             } else {
-                return QVariant();
+                return QVariant(); // Return no decoration
             }
         }
-        default: return QVariant();
+        default: return QVariant(); // Unhandled role
     }
 
 }
 
 QVariant ResauceFileModel::headerData(int section, Qt::Orientation, int role) const {
 
-    if (role != Qt::DisplayRole) {
+    if (role != Qt::DisplayRole) { // We only handle display role right now
         return QVariant();
     }
 
-    switch (section) {
+    switch (section) { // Custom model headers
         case 0: return "Name";
         case 1: return "New Name";
         case 2: return "Size";
