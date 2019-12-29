b31151d115b1c6f94efa990da706652a3974b916
George-Lewis <georgejeffreylewis@gmail.com>
2019-12-29 09:32:30 -0500

    Flesh out process func

--- src/resaucer.cpp
+++ src/resaucer.cpp
@@ -1,6 +1,51 @@
 #include "resaucer.h"
 
-Resaucer::Resaucer()
-{
+Resaucer::Resaucer() {}
+
+void Resaucer::process() {
+
+    for (auto file : files) {
+
+        auto name = file.fileName();
+
+        auto ext = file.fileExtension();
+
+        QHash<QString, QString> extracted;
+
+        QString output = _template;
+
+        for (auto regex : extractors) {
+
+            auto match = regex.match(name);
+
+            if (match.hasMatch()) {
+
+                for (auto group : regex.namedCaptureGroups()) {
+
+                    extracted[group] = match.captured(group);
+
+                }
+
+            }
+
+        }
+
+        for (auto group : extracted.keys()) {
+
+            output = output.replace("{"+group+"}", extracted[group]);
+
+        }
+
+        for (auto var : vars) {
+
+            output = output.replace("{" + var.name + "}", QString::number(var.value));
+
+            var.value += var.incdec;
+
+        }
+
+        file.new_name = output;
+
+    }
 
 }
--- src/resaucer.h
+++ src/resaucer.h
@@ -2,7 +2,9 @@
 #define RESAUCER_H
 
 #include "resaucefileinfo.h"
+#include "ResauceVariable.h"
 
+#include <QRegularExpression>
 #include <QVector>
 
 class Resaucer
@@ -13,8 +15,13 @@ class Resaucer
 public:
 
     QVector<ResauceFileInfo> files;
+    QVector<ResauceVariable> vars;
+    QVector<QRegularExpression> extractors;
+    QString _template;
 
     Resaucer();
+
+    void process();
 
 };
 
